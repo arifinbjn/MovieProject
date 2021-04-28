ca4d60203e977cc6cd8e92b0a2916d5f8cd995c9
Muhamad Arifin <arifin@smktelkom-mlg.sch.id>
2021-04-28 09:22:10 +0700

    commit projeck

---
+++ app/src/main/java/www/smktelkommlg/sch/id/movie/MovieService.java
@@ -0,0 +1,14 @@
+package www.smktelkommlg.sch.id.movie;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface MovieService {
+
+//@GET("/JSONParsingTutorial/jsonMovie")
+//Call<MovieService> getMovies();
+
+    //merubah kode menjadi seperi berikut
+    @GET("/")
+    Call<MovieResponse> getMovies();
+}
